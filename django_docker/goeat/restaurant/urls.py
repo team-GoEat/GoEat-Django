7af7c4c46d53202ec9ac864bf3c3b4fb00f15ac5
plm3332 <plm3332@gmail.com>
2021-08-18 17:18:02 +0900

    Restore Restaurant List Serializer, URL, and Viewset

--- django_docker/goeat/restaurant/urls.py
+++ django_docker/goeat/restaurant/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from restaurant import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('list', views.ResView)
 
 urlpatterns = [
+    # 음식점 정보
+    path('', include(router.urls)),
     # 메뉴로 식당 검색
     path('menu/<menu_id>/', views.get_restaurant_by_menu, name='get_restaurant_by_menu'),
     # 식당 검색
--- django_docker/goeat/restaurant/views.py
+++ django_docker/goeat/restaurant/views.py
@@ -3,7 +3,12 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from restaurant.models import Restaurant, Menu
-from restaurant.serializers import RestaurantSerializer, MenuSerializer, SimpleRestaurantSerializer, SimpleMenuSerializer
+from restaurant.serializers import MenuSerializer, SimpleRestaurantSerializer, SimpleMenuSerializer, RestaurantSerializer
+
+# 음식점 정보
+class ResView(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all().order_by('id')
+    serializer_class = RestaurantSerializer
 
 # 메뉴로 식당 검색
 @api_view(['GET'])
@@ -17,7 +22,7 @@ def get_restaurant_by_menu(request, *args, **kwargs):
 
     try:
         restaurants = Restaurant.objects.get(menu = menu)
-        serializer = RestaurantSerializer(restaurants, many=True)
+        serializer = SimpleRestaurantSerializer(restaurants, many=True)
         return Response(serializer.data, status=200)
     except Restaurant.DoesNotExist:
         return JsonResponse({'msg': '식당이 없습니다.'}, status=status.HTTP_400_BAD_REQUEST, json_dumps_params={'ensure_ascii':True})
