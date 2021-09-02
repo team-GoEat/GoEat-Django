471b2a0e2507f628888575bdc18a82d7d8c891b5
plm3332 <plm3332@gmail.com>
2021-09-02 15:25:21 +0900

    Remove NoticeHtml Field in Notice Model, Change NoticeContent Field to TextField

--- django_docker/goeat/notice/models.py
+++ django_docker/goeat/notice/models.py
@@ -12,9 +12,7 @@ class Notice(models.Model):
     # 공지 제목
     notice_title = models.CharField(max_length=30, default='title')
     # 공지 내용
-    notice_content = models.CharField(max_length=300, default='content')
-    # 공지 HTML
-    notice_html = models.TextField(blank=True, null=True)
+    notice_content = models.TextField(blank=True, null=True)
     # 공지 날짜
     notice_date = models.DateTimeField(auto_now_add=True)
 
