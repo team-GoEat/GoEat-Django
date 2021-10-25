863da352c863425075e054fa6ca2bd7141129bdb
plm3332 <plm3332@gmail.com>
2021-10-25 10:40:52 +0900

    Added robots.txt and it's Path

--- goeat/goeat/urls.py
+++ goeat/goeat/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,7 +27,11 @@ urlpatterns = [
     path('notice/', include('notice.urls')),
     path('restaurant/', include('restaurant.urls')),
 
+    # favicon path
     path("favicon.ico", RedirectView.as_view(url='/static/favicon.ico')),
+
+    # robots.txt path
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
