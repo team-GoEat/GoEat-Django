f19a11239209ef46d0ac1d748fa0620c15d5abb8
박건희 <baggeonhui@baggeonhuiui-Macmini.local>
2021-11-29 14:58:31 +0900

    쿠폰 등록하기 로직 추가

--- goeat/static/app_owner/base/js/utility.js
+++ goeat/static/app_owner/base/js/utility.js
@@ -1,4 +1,4 @@
-export function inputPhoneNumber(value) {
+function inputPhoneNumber(value) {
   let number = value.replace(/[^0-9]/g, "");
   let phone = "";
 
@@ -15,6 +15,27 @@ export function inputPhoneNumber(value) {
     phone += number.substr(0, 3);
     phone += "-"; phone += number.substr(3, 4);
     phone += "-"; phone += number.substr(7);
+  }
+  return phone;
+}
+function inputDateNumber(value) {
+  let number = value.replace(/[^0-9]/g, "");
+  let phone = "";
+
+  if (number.length < 3) { return number; }
+  else if (number.length < 4) {
+    phone += number.substr(0, 2);
+    phone += "."; phone += number.substr(2);
+  }
+  else if (number.length < 6) {
+    phone += number.substr(0, 2);
+    phone += "."; phone += number.substr(2, 2);
+    phone += "."; phone += number.substr(4);
+  }
+  else {
+    phone += number.substr(0, 2);
+    phone += "."; phone += number.substr(2, 2);
+    phone += "."; phone += number.substr(4);
   }
   return phone;
 }
