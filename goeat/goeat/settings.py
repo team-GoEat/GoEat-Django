1a735f688f31d40af5c59c2c5fa69e1b9a84df03
plm3332 <plm3332@gmail.com>
2021-12-06 16:48:37 +0900

    django setting

--- goeat/goeat/settings.py
+++ goeat/goeat/settings.py
@@ -41,7 +41,10 @@ for key, value in secrets.items():
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DJANGO_DEBUG', False):
+    DEBUG = False
+else:
+    DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
@@ -117,16 +120,12 @@ MIDDLEWARE = [
 
 ####### HOSTS SETTING ( 변경 금지 )
 
-# ROOT_URLCONF = 'goeat.urls'
 ROOT_HOSTCONF = 'goeat.hosts'
-ROOT_URLCONF = 'app_owner.urls'
-# DEFAULT_HOST = 'owner'
-DEFAULT_HOST = 'owner'
+ROOT_URLCONF = 'goeat.urls'
+DEFAULT_HOST = 'www'
 
-print(DEBUG)
 if DEBUG:
     PARENT_HOST = 'test.me:8000'
-    # PARENT_HOST = '127.0.0.1:8000'
 else:
     PARENT_HOST = 'goeat.kr'
 
@@ -149,33 +148,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'goeat.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {}
-
-# DATABASES['default'] = {
-#     "ENGINE" : "django.db.backends.mysql",
-#     "NAME" : "goeat_db",
-#     "USER" : "goeat",
-#     "PASSWORD" : "goeat@9797",
-#     "HOST" : "mysql",
-#     "PORT" : "3306",
-#     "OPTIONS": {
-#         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         'charset': 'utf8mb4', 
-#         'use_unicode': True,
-#     },
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
