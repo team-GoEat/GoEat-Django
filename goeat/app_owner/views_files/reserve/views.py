c85028a33e419b2e82a1e1b5cf806636e46b0413
권순성 <tnstjd120@gmail.com>
2021-12-22 13:10:37 +0900

    Reserve row setting2

--- goeat/app_owner/views_files/reserve/views.py
+++ goeat/app_owner/views_files/reserve/views.py
@@ -7,6 +7,7 @@ import datetime
 from datetime import timedelta
 
 from accounts.models import ResReservationRequest
+from app_owner.views_files.reserve.callback import make_reserve
 
 class Views_Controls(View):
 
@@ -14,9 +15,25 @@ class Views_Controls(View):
 
         reservation = ResReservationRequest.objects.filter(receiver_id=request.session['res_id']).order_by('-is_active')
 
+        ResReservationRequest.objects.filter(receiver_id=request.session['res_id']).update(is_view=True)
+
         context = {
             'reservation': reservation,
-
         }
 
         return render(request, 'app_owner/reserve/index.html', context)
+
+class Views_Controls2(View):
+
+    def post(self, request):
+
+        result = ''
+
+        reservation = ResReservationRequest.objects.filter(receiver_id=request.session['res_id'],is_view=False)
+
+        for item in reservation:
+            result += make_reserve(request,item).content.decode('UTF-8')
+            item.is_view = True
+            item.save()
+
+        return HttpResponse(result)
