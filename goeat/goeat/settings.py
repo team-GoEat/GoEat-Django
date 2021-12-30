d809a3695429aefd81b05d28c5f46dfeb8938409
plm3332 <plm3332@gmail.com>
2021-12-30 11:27:38 +0900

    Media Urls Deleted

--- goeat/goeat/settings.py
+++ goeat/goeat/settings.py
@@ -269,8 +269,8 @@ STATIC_ROOT = ''
 STATICFILES_DIRS = (os.path.join('static'), )
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
