6a048e1ef8f753cd0a9a8f92b8b87931fd64d26f
권순성 <tnstjd120@gmail.com>
2021-12-31 17:39:58 +0900

    cron tab update

--- goeat/goeat/cron.py
+++ goeat/goeat/cron.py
@@ -4,6 +4,6 @@ import datetime
 def RestaurantPosCron():
 
     now = datetime.datetime.now()
-    check_dttm = now - datetime.timedelta(minutes=1)
+    check_dttm = now - datetime.timedelta(seconds=20)
 
-    res_data = Restaurant.objects.filter(is_reservable_r=True, res_pos_time__lt=check_dttm).update(is_reservable_r=False)
+    Restaurant.objects.filter(is_reservable_r=True, res_pos_time__lt=check_dttm).update(is_reservable_r=False)
