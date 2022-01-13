1852181147ad32e0adeab5c82e6ddef84bf68197
권순성 <tnstjd120@gmail.com>
2022-01-13 18:02:30 +0900

    coupon setting2

--- goeat/app_owner/views_files/coupon/create_coupon.py
+++ goeat/app_owner/views_files/coupon/create_coupon.py
@@ -4,14 +4,28 @@ from django.views import View
 from django.urls import reverse
 
 from restaurant.model_files.coupon import ResCoupon
+from querystring_parser import parser
 
-import datetime
+import datetime, json
+
 
 class Views_Controls(View):
     def post(self, request):
 
-        print(request.POST)
-        print(request.POST.getlist('couponList[]'))
+        for k, v in parser.parse(request.POST.urlencode())['coupon_list'].items():
+            coupon_start_dttm = v['coupon_start_dttm']
+            
+            print(coupon_start_dttm)
+            print(request.POST['coupon_type'])
+
+            # ResCoupon.objects.create(
+            #     restaurant_id = request.session['res_id'],
+            #     coupon_type = request.POST['coupon_type'],
+            #     coupon_content = v['coupon_content'],
+            #     coupon_count = v['coupon_count'],
+            #     coupon_start_dttm = ,
+                
+            # )
 
         context = {}
 
