12da8a608a4c31b29e62c782334623d60940d6fd
이진환 <ijinhwan@ijinhwan-ui-MacBookPro.local>
2022-01-13 18:02:15 +0900

    글로벌 변수

--- goeat/app_owner/views_files/stamp/views.py
+++ goeat/app_owner/views_files/stamp/views.py
@@ -1,19 +1,35 @@
+from tracemalloc import start
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.views import View
 from django.urls import reverse
 
 from accounts.model_files.stamp import StampLog
+import datetime
 
 class Views_Controls(View):
     def post(self, request):
 
         type = request.POST.get('type','')
-
+        
         if type == '':
-            
-            context = {
 
+            start_dttm = request.POST.get('start_dttm','')
+            end_dttm = request.POST.get('end_dttm','')
+            
+            if start_dttm:
+                start_dttm = datetime.datetime.strptime(start_dttm,'%Y.%m.%d %H:%M:%S')
+            else:
+                start_dttm = datetime.datetime.now().replace(hour=0,minute=0,second=0)
+
+            if end_dttm:
+                end_dttm = datetime.datetime.strptime(end_dttm,'%Y.%m.%d %H:%M:%S')
+            else:
+                end_dttm = datetime.datetime.now().replace(hour=0,minute=0,second=0)
+
+            context = {
+                'start_dttm':datetime.datetime.strftime(start_dttm, '%Y-%m-%d').replace('-', '.'),
+                'end_dttm':datetime.datetime.strftime(end_dttm, '%Y-%m-%d').replace('-', '.')
             }
 
             return render(request, 'app_owner/stamp/index.html', context)
