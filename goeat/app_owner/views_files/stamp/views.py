aea7da2a345c29588214ae10c50da1f33be98e82
이진환 <ijinhwan@ijinhwan-ui-MacBookPro.local>
2022-01-19 22:17:00 +0900

    임시체크 ; ;

--- goeat/app_owner/views_files/stamp/views.py
+++ goeat/app_owner/views_files/stamp/views.py
@@ -11,8 +11,14 @@ class Views_Controls(View):
     def post(self, request):
 
         type = request.POST.get('type','')
-        
+
         if type == '':
+
+            min_price = 200000
+            commission = min_price * 0.03
+
+            days_count = 0
+            month_count = 0
 
             start_dttm = request.POST.get('start_dttm','')
             end_dttm = request.POST.get('end_dttm','')
@@ -27,9 +33,22 @@ class Views_Controls(View):
             else:
                 end_dttm = datetime.datetime.now().replace(hour=0,minute=0,second=0)
 
+
+            for item in StampLog.objects.filter(stamp_create_dttm__range=(start_dttm,end_dttm),stamp__restaurant_id=request.session['res_id']):
+                days_count += item.stamp_own
+
+            for item in StampLog.objects.filter(stamp_create_dttm__icontains=datetime.datetime.now().strftime('%Y-%m'),stamp__restaurant_id=request.session['res_id']):
+                month_count += item.stamp_own
+
             context = {
                 'start_dttm':datetime.datetime.strftime(start_dttm, '%Y-%m-%d').replace('-', '.'),
-                'end_dttm':datetime.datetime.strftime(end_dttm, '%Y-%m-%d').replace('-', '.')
+                'end_dttm':datetime.datetime.strftime(end_dttm, '%Y-%m-%d').replace('-', '.'),
+                'month':datetime.datetime.now().strftime('%m'),
+                'days_count':days_count,
+                'days_point': int(days_count * commission),
+                'month_count':month_count,
+                'month_point':int(month_count * commission),
+                'min_price':min_price
             }
 
             return render(request, 'app_owner/stamp/index.html', context)
@@ -37,7 +56,7 @@ class Views_Controls(View):
         elif type == 'list':
             
             context = {
-                'stamplogs' : StampLog.objects.filter(stamp__restaurant_id=request.session['res_id']).order_by('-stamp_create_dttm')
+                'stamplogs' : StampLog.objects.filter(stamp__restaurant_id=request.session['res_id']).order_by('-stamp_create_dttm')[:50]
             }
 
             return render(request, 'app_owner/stamp/list.html', context)
