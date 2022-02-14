bbd81019c82cb9c86d77b467a8fdcae2a9063e4d
plm3332 <plm3332@gmail.com>
2022-02-14 16:24:28 +0900

    UserAttributesSimilarityValidator Disabled

--- goeat/goeat/settings.py
+++ goeat/goeat/settings.py
@@ -142,9 +142,9 @@ WSGI_APPLICATION = 'goeat.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
